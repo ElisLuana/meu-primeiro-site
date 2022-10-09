d6e9abe4b0f1bbd7abba943c46e66c106c3b058e
Elis Luana Dias <elisluana2019@gmail.com>
2022-10-08 21:54:32 -0300

    codigo fonte
    Codigo com minhas alterações

---
+++ Site ProgrAmo/Script.js
@@ -0,0 +1,11 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+  if(document.getElementById("nome").value != "" && 
+     document.getElementById("email").value != "" && 
+     document.getElementById("telefone").value != "" ){
+      alert("Prontinho! Voce recebera as novidades por email.")
+  }else{
+      alert("Por favor, preencha os campo de nome, email e telefone.")
+  }
+}
